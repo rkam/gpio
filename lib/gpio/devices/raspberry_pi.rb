a58e34fec60da89d8a9298a696aed63fac36e90b
rkam <rkamicar@gmail.com>
2016-05-21 01:37:32 -0700

    A HW pin was wrong and had 40pin HW/SW swapped

--- lib/gpio/devices/raspberry_pi.rb
+++ lib/gpio/devices/raspberry_pi.rb
@@ -14,8 +14,8 @@ module GPIO
     DIRECTION_FILE = "direction"
     VALUE_FILE     = "value"
 
-    HARDWARE_PINS = [ 3,5,7, 8,10,11,12,13,15,16,17,19,21,22,23,24,26 ]
-    SOFTWARE_PINS = [ 0,1,4,14,15,17,18,21,22,23,24,10, 9,25,11, 8, 7 ]
+    HARDWARE_PINS = [ 3, 5, 7,  8, 10, 11, 12, 13, 15, 16, 18, 19, 21, 22, 23, 24, 26 ]
+    SOFTWARE_PINS = [ 0, 1, 4, 14, 15, 17, 18, 21, 22, 23, 24, 10,  9, 25, 11,  8,  7 ]
 
     # create: /sys/class/gpio/export   N
     # delete: /sys/class/gpio/unexport N
--- lib/gpio/devices/raspberry_pi_revision_2.rb
+++ lib/gpio/devices/raspberry_pi_revision_2.rb
@@ -14,8 +14,8 @@ module GPIO
     DIRECTION_FILE = "direction"
     VALUE_FILE     = "value"
 
-    HARDWARE_PINS = [ 3,5,7, 8,10,11,12,13,15,16,17,19,21,22,23,24,26 ]
-    SOFTWARE_PINS = [ 2,3,4,14,15,17,18,27,22,23,24,10, 9,25,11, 8, 7 ]
+    HARDWARE_PINS = [ 3, 5, 7,  8, 10, 11, 12, 13, 15, 16, 18, 19, 21, 22, 23, 24, 26 ]
+    SOFTWARE_PINS = [ 2, 3, 4, 14, 15, 17, 18, 27, 22, 23, 24, 10,  9, 25, 11,  8,  7 ]
     # v1 diff:        0 1               21
 
     # For reference - Unused
